5d36447e88949949bf91f3f92bceadc859e605cb
DeriMuhamad29 <derihandipa17@gmail.com>
2022-08-13 04:08:20 +0700

    first commit

---
+++ UAS-AKB IF-7 10119292/UAS-AKB-IF7-10119292/app/src/main/java/com/deri10119292/sqlitecrud/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.deri10119292.sqlitecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
